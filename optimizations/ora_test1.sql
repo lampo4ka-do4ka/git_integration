76b8fdcd4775f0dae075f38664ef6d0e61d81efb
admin <admin@datavirtuality>
2021-01-19 15:18:46 +0300

    Optimization Job 78bce161-5a50-11eb-aebd-0a0027000015 has been created.

--- optimizations/ora_test1.sql
+++ optimizations/ora_test1.sql
@@ -4,4 +4,5 @@ BEGIN
     CALL SYSADMIN.setOptimizationFrequency(optimizationId => optId, frequency => 1);
     CALL SYSADMIN.setRecOptAllowIndexCreationByType(recOptId => optId, allowIndexCreationByType => 'ALL');
     CALL SYSADMIN.setRecOptAllowIndexCreationByStatus(recOptId => optId, allowIndexCreationByStatus => 'NEW_ACCEPTED');
+    CALL SYSADMIN.createOptimizationJob(recOptId => optId, allowIndexCreation => 'ALL', gatherNativeStats => true, indexCreationByStatus => 'NEW_ACCEPTED', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, jobName => '78bce161-5a50-11eb-aebd-0a0027000015');
 END;;
