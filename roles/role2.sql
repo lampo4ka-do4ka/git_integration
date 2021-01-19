ebbca8006cad56bbb3ec96c2c90fd0294b77bae8
admin <admin@datavirtuality>
2021-01-19 13:10:28 +0300

    Permission ROLE2::A has been created.

--- roles/role2.sql
+++ roles/role2.sql
@@ -1,1 +1,2 @@
 CALL SYSADMIN.addRole(name => 'role2', allowCreateTempTables  => false);;
+CALL SYSADMIN.setPermissions(role_name => 'role2', resourceName => 'a', permissions => 'CRUDEAL', isConstraint => true);;
