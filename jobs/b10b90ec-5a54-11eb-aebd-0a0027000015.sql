5411d17b3f9efadba8f7322dce6323669cd87cb4
admin <admin@datavirtuality>
2021-01-19 15:49:09 +0300

    Job has been dropped.

--- jobs/b10b90ec-5a54-11eb-aebd-0a0027000015.sql
+++
@@ -1,4 +0,0 @@
-BEGIN
- DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select * from "pg.test1";; select 1;; ', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => 'x', jobName => 'b10b90ec-5a54-11eb-aebd-0a0027000015') OPTION $NOFAIL;
-END;;
