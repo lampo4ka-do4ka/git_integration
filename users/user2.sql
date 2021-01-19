d2d92de0c6d21a0636697c97b0f9e25be53f787b
admin <admin@datavirtuality>
2021-01-19 16:23:20 +0300

    User user2 has been renamed to user21.

--- users/user2.sql
+++
@@ -1,3 +0,0 @@
-CALL SYSADMIN.importUser(name => 'user2', encryptedPwd => '4TNj0DF02EY=', role_name => 'connect-dv-role');;
-CALL SYSADMIN.addUserRole(user_name => 'user2', role_name => 'super-admin-role');;
-CALL SYSADMIN.addUserRole(user_name => 'user2', role_name => 'role2');;
