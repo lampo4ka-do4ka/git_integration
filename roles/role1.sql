c5fc7c748bab0e7caeec66eba7e5f6ba53cf186f
admin <admin@datavirtuality>
2021-01-19 13:09:42 +0300

    Permission ROLE1::RESOURCE has been created.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,2 +1,3 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
 CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', isConstraint => true);;
+CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'resource', permissions => 'CRUDEAL', isConstraint => true);;
