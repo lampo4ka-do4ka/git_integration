614f1d084e555e0499ad686de4980223d1fda91d
admin <admin@datavirtuality>
2021-01-19 13:09:37 +0300

    Permission ROLE1::PG.TEST1 has been created.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,1 +1,2 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
+CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', isConstraint => true);;
