9b0cb49c10879fd52ec88f2831a7dcdd77f63349
admin <admin@datavirtuality>
2021-01-19 16:12:45 +0300

    Schedule eec598bd-5a51-11eb-aebd-0a0027000015 has been dropped.

--- jobs/ec00ac5c-5a51-11eb-aebd-0a0027000015.sql
+++ jobs/ec00ac5c-5a51-11eb-aebd-0a0027000015.sql
@@ -1,5 +1,4 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
     jobId = CALL SYSADMIN.CreateSQLJob(script => 'exec UTILS.upsert(source_table => ''"pg.test1"'',keyColumnsArray => array(''a''),updateColumns => array(''b''),invertUpdateColumns => false,target_table => ''"dwh.test_uu_pg"'',surrogateKeyType => NULL,surrogateKeyName => NULL,dbmsTableCreationOptions => NULL,checkMaxField => NULL,defaultvalueIfCheckMaxFieldIsNull => NULL)', parallelRunsAllowed => 1, retryCounter => 3, retryDelay => 30, runTimeout => 0, description => 'Upsert update from "pg.test1" to "dwh.test_uu_pg"', jobName => 'ec00ac5c-5a51-11eb-aebd-0a0027000015') OPTION $NOFAIL;
-    CALL SYSADMIN.createSchedule(jobId => jobId, type => 'CRON', startDelay => 0, enabled => true, cronExpression => '0 0 12 * * ?', scheduleName => 'eec598bd-5a51-11eb-aebd-0a0027000015');
 END;;
