1bdb0e3b1ec20c285eb0f517e725303b0d2be4a5
admin <admin@datavirtuality>
2021-01-19 16:18:12 +0300

    Job has been dropped.

--- jobs/8c78f445-5a51-11eb-aebd-0a0027000015.sql
+++
@@ -1,4 +0,0 @@
-BEGIN
- DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.CreateSQLJob(script => 'exec SYSADMIN.BatchUpdate(''"dwh.test_bu_pg"'',''"ora.test1"'',NULL)', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => 'Batch replication from "ora.test1" to "dwh.test_bu_pg" test', jobName => '8c78f445-5a51-11eb-aebd-0a0027000015') OPTION $NOFAIL;
-END;;
