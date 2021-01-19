a9bd85f62e99465c2f2ec20d443941c204e237ea
admin <admin@datavirtuality>
2021-01-19 16:23:51 +0300

    Role role1 has been renamed to role12.

--- roles/role1.sql
+++
@@ -1,4 +0,0 @@
-CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v1', permissions => 'CRUDE', condition => 'a = 1', isConstraint => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', condition => '', isConstraint => true, mask => '');;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v3', permissions => 'CRUDEA', condition => '', isConstraint => false, mask => '');;
