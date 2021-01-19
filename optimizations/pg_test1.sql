aa423c2eab4622c912097ee96c8423252d68255e
admin <admin@datavirtuality>
2021-01-19 15:19:55 +0300

    Schedule a2bd2384-5a50-11eb-aebd-0a0027000015 has been created.

--- optimizations/pg_test1.sql
+++ optimizations/pg_test1.sql
@@ -5,4 +5,5 @@ BEGIN
     CALL SYSADMIN.setRecOptAllowIndexCreationByType(recOptId => optId, allowIndexCreationByType => 'ALL');
     CALL SYSADMIN.setRecOptAllowIndexCreationByStatus(recOptId => optId, allowIndexCreationByStatus => 'NEW_ACCEPTED');
     CALL SYSADMIN.createOptimizationJob(recOptId => optId, allowIndexCreation => 'ALL', gatherNativeStats => true, indexCreationByStatus => 'NEW_ACCEPTED', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, jobName => 'a2b11593-5a50-11eb-aebd-0a0027000015');
+    CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='a2b11593-5a50-11eb-aebd-0a0027000015'), type => 'INTERVAL', startDelay => 1, enabled => true, intervl => 60, scheduleName => 'a2bd2384-5a50-11eb-aebd-0a0027000015');
 END;;
