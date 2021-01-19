6515a396dc4f0896ea02f56328436a8715508b0b
admin <admin@datavirtuality>
2021-01-19 16:17:32 +0300

    Optimization Job 0390a010-5a52-11eb-aebd-0a0027000015 has been dropped.

--- optimizations/pg_test1.sql
+++ optimizations/pg_test1.sql
@@ -6,7 +6,6 @@ BEGIN
     CALL SYSADMIN.setRecOptAllowIndexCreationByStatus(recOptId => optId, allowIndexCreationByStatus => 'NEW_ACCEPTED');
     CALL SYSADMIN.createOptimizationJob(recOptId => optId, allowIndexCreation => 'ALL', gatherNativeStats => true, indexCreationByStatus => 'NEW_ACCEPTED', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, jobName => 'a2b11593-5a50-11eb-aebd-0a0027000015');
     CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='a2b11593-5a50-11eb-aebd-0a0027000015'), type => 'INTERVAL', startDelay => 1, enabled => true, intervl => 60, scheduleName => 'a2bd2384-5a50-11eb-aebd-0a0027000015');
-    CALL SYSADMIN.createOptimizationJob(recOptId => optId, allowIndexCreation => 'ALL', gatherNativeStats => true, indexCreationByStatus => 'NEW_ACCEPTED', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, jobName => '0390a010-5a52-11eb-aebd-0a0027000015');
     CALL SYSADMIN.createIncrementalOptimizationJob(recOptId => optId, newRowCheckExpression => '"a"', identityExpression => 'null', deleteOldData => true, gatherNativeStats => false, parallelRunsAllowed => 1, retryCounter => 3, retryDelay => 30, runTimeout => 0, jobName => '14b3fc22-5a52-11eb-aebd-0a0027000015');
     CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='14b3fc22-5a52-11eb-aebd-0a0027000015'), type => 'INTERVAL', startDelay => 1, enabled => true, intervl => 120, scheduleName => '19f02a13-5a52-11eb-aebd-0a0027000015');
 END;;
