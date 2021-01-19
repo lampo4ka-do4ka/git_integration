6db572d1703ea2a60b0e565b42d359d1d4ed5936
admin <admin@datavirtuality>
2021-01-19 16:17:35 +0300

    Schedule 19f02a13-5a52-11eb-aebd-0a0027000015 has been dropped.

--- optimizations/pg_test1.sql
+++ optimizations/pg_test1.sql
@@ -5,5 +5,4 @@ BEGIN
     CALL SYSADMIN.setRecOptAllowIndexCreationByType(recOptId => optId, allowIndexCreationByType => 'ALL');
     CALL SYSADMIN.setRecOptAllowIndexCreationByStatus(recOptId => optId, allowIndexCreationByStatus => 'NEW_ACCEPTED');
     CALL SYSADMIN.createIncrementalOptimizationJob(recOptId => optId, newRowCheckExpression => '"a"', identityExpression => 'null', deleteOldData => true, gatherNativeStats => false, parallelRunsAllowed => 1, retryCounter => 3, retryDelay => 30, runTimeout => 0, jobName => '14b3fc22-5a52-11eb-aebd-0a0027000015');
-    CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='14b3fc22-5a52-11eb-aebd-0a0027000015'), type => 'INTERVAL', startDelay => 1, enabled => true, intervl => 120, scheduleName => '19f02a13-5a52-11eb-aebd-0a0027000015');
 END;;
