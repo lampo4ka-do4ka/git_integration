91e5b3d1d38b0abe2679d7d8cb39b65666724f91
admin <admin@datavirtuality>
2021-01-19 13:09:52 +0300

    Permission ROLE1::RESOURCE has been dropped.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,3 +1,2 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
 CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', isConstraint => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'resource', permissions => 'CRUDEAL', isConstraint => true);;
