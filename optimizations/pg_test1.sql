02781ba80aedc3cd605ed92d3ed157e8bfa2313c
admin <admin@datavirtuality>
2021-01-19 16:17:36 +0300

    Optimization Job 14b3fc22-5a52-11eb-aebd-0a0027000015 has been dropped.

--- optimizations/pg_test1.sql
+++ optimizations/pg_test1.sql
@@ -4,5 +4,4 @@ BEGIN
     CALL SYSADMIN.setOptimizationFrequency(optimizationId => optId, frequency => 1);
     CALL SYSADMIN.setRecOptAllowIndexCreationByType(recOptId => optId, allowIndexCreationByType => 'ALL');
     CALL SYSADMIN.setRecOptAllowIndexCreationByStatus(recOptId => optId, allowIndexCreationByStatus => 'NEW_ACCEPTED');
-    CALL SYSADMIN.createIncrementalOptimizationJob(recOptId => optId, newRowCheckExpression => '"a"', identityExpression => 'null', deleteOldData => true, gatherNativeStats => false, parallelRunsAllowed => 1, retryCounter => 3, retryDelay => 30, runTimeout => 0, jobName => '14b3fc22-5a52-11eb-aebd-0a0027000015');
 END;;
