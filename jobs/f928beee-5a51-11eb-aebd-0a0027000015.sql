fe8f6d5a70c6e19ccdb84aac09509c4fbc38c331
admin <admin@datavirtuality>
2021-01-19 16:18:28 +0300

    Schedule fc66f9ff-5a51-11eb-aebd-0a0027000015 has been dropped.

--- jobs/f928beee-5a51-11eb-aebd-0a0027000015.sql
+++ jobs/f928beee-5a51-11eb-aebd-0a0027000015.sql
@@ -1,5 +1,4 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
     jobId = CALL SYSADMIN.createCopyOverSourceTableJob(sourceTable => 'pg.test1', targetTable => 'dwh.test_co_pg', cleanupMethod => 'TRUNCATE', description => 'sourceTable=pg.test1,targetTable=dwh.test_co_pg,cleanupMethod=TRUNCATE,description=Copy pg.test1 over dwh.test_co_pg,refreshTarget=true,allowsAlterTableStatements=false', refreshTarget => true, allowsAlterTableStatements => false, jobName => 'f928beee-5a51-11eb-aebd-0a0027000015', retryCounter => 0, retryDelay => 0, runTimeout => 0 );
-    CALL SYSADMIN.createSchedule(jobId => jobId, type => 'CRON', startDelay => 0, enabled => true, cronExpression => '0 0 12 ? * SUN,MON', scheduleName => 'fc66f9ff-5a51-11eb-aebd-0a0027000015');
 END;;
