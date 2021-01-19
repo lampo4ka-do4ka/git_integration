99007f2422e767f4631f13c25a69e8a8c52978de
admin <admin@datavirtuality>
2021-01-19 16:19:35 +0300

    Virtual Schema has been dropped.

--- virtual_schemas/d/procedures/remarks.sql
+++
@@ -1,3 +0,0 @@
-
-CALL SYSADMIN.setRemark(name => 'D.P2', remark => 'test description') OPTION $NOFAIL;;
-CALL SYSADMIN.setRemark(name => 'D.P5', remark => 'test description 5') OPTION $NOFAIL;;
--- virtual_schemas/d/views/remarks.sql
+++
@@ -1,2 +0,0 @@
-
-CALL SYSADMIN.setRemark(name => 'D.V2', remark => 'test description') OPTION $NOFAIL;;
