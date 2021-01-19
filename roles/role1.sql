5eca1a4467b3d8e524550227fb1b4a635812f256
admin <admin@datavirtuality>
2021-01-19 13:09:36 +0300

    Permission ROLE1::* has been dropped.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,2 +1,1 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => '*', permissions => 'CRUDEAL', isConstraint => true);;
