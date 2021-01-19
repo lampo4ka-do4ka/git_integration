a99c3282873e2c1ce24e2683db1dd083609a3b94
admin <admin@datavirtuality>
2021-01-19 16:18:27 +0300

    Job a95ce3a7-5a51-11eb-aebd-0a0027000015 has been dropped.

--- jobs/a95ce3a7-5a51-11eb-aebd-0a0027000015.sql
+++
@@ -1,4 +0,0 @@
-BEGIN
- DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.createCopyOverSourceTableJob(sourceTable => 'ora.test1', targetTable => 'dwh.test_co_ora', cleanupMethod => 'DELETE', description => 'sourceTable=ora.test1,targetTable=dwh.test_co_ora,cleanupMethod=DELETE,description=Copy ora.test1 over dwh.test_co_ora,refreshTarget=true,allowsAlterTableStatements=false', refreshTarget => true, allowsAlterTableStatements => false, jobName => 'a95ce3a7-5a51-11eb-aebd-0a0027000015', retryCounter => 0, retryDelay => 0, runTimeout => 0 );
-END;;
