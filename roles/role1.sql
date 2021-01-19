7ac35f0a14616a5aa5463796503e1d0f058dee74
admin <admin@datavirtuality>
2021-01-19 13:09:55 +0300

    Permission ROLE1::VIEWS.V1 has been changed.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,3 +1,3 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
 CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', isConstraint => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v1', permissions => 'CRUDEA', isConstraint => true);;
+CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v1', permissions => 'CRUDE', isConstraint => true);;
