4738a33661bfb93c9f94c70b29253403bd5d7032
admin <admin@datavirtuality>
2021-01-19 15:44:35 +0300

    Job has been renamed to fe8497f9-5a53-11eb-aebd-0a0027000015.

--- jobs/fe8497f9-5a53-11eb-aebd-0a0027000015.sql
+++ jobs/fe8497f9-5a53-11eb-aebd-0a0027000015.sql
@@ -1,4 +1,4 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select * from "pg.test1" ', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => 'x', jobName => 'fe8497f9-5a53-11eb-aebd-0a0027000015') OPTION $NOFAIL;
+    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select a from "pg.test1" ', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => 'x', jobName => 'fe8497f9-5a53-11eb-aebd-0a0027000015') OPTION $NOFAIL;
 END;;
