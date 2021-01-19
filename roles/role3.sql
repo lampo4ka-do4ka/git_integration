59db1be25cc48200459bb470a61bcb862e1402a4
admin <admin@datavirtuality>
2021-01-19 16:26:49 +0300

    Role has been dropped.

--- roles/role3.sql
+++
@@ -1,3 +0,0 @@
-CALL SYSADMIN.addRole(name => 'role3', allowCreateTempTables  => false);;
-CALL SYSADMIN.setPermissions(role_name => 'role3', resourceName => 'c.p1', permissions => 'CRUEAL', isConstraint => false);;
-CALL SYSADMIN.setPermissions(role_name => 'role3', resourceName => 'resource', permissions => 'CRUDEAL', isConstraint => true);;
