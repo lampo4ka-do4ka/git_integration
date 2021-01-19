e8b17b256404d31ea5b95ab608c4b37cdfea3c4f
admin <admin@datavirtuality>
2021-01-19 13:27:12 +0300

    A Data Catalog Attribute for views.v3 has been set.

--- data-catalog.sql
+++ data-catalog.sql
@@ -1,2 +1,3 @@
 CALL SYSADMIN.setDataCatalogAttribute(name => 'c.p1', attribute => 'owner', attrValue => 'user1');;
 CALL SYSADMIN.setDataCatalogAttribute(name => 'views.v1', attribute => 'owner', attrValue => 'user1');;
+CALL SYSADMIN.setDataCatalogAttribute(name => 'views.v3', attribute => 'owner', attrValue => 'user3');;
