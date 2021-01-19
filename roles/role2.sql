9768ea369ebb8cbef9b303f5dd087128a0b0f8df
admin <admin@datavirtuality>
2021-01-19 13:10:30 +0300

    Permission ROLE2::A has been changed.

--- roles/role2.sql
+++ roles/role2.sql
@@ -1,2 +1,2 @@
 CALL SYSADMIN.addRole(name => 'role2', allowCreateTempTables  => false);;
-CALL SYSADMIN.setPermissions(role_name => 'role2', resourceName => 'a', permissions => 'RUDEAL', isConstraint => true);;
+CALL SYSADMIN.setPermissions(role_name => 'role2', resourceName => 'a', permissions => 'UDEAL', isConstraint => true);;
