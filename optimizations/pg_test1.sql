a56608428d41db1ef3af7284d5a94c9e616bc704
admin <admin@datavirtuality>
2021-01-19 15:30:24 +0300

    Schedule 19f02a13-5a52-11eb-aebd-0a0027000015 has been created.

--- optimizations/pg_test1.sql
+++ optimizations/pg_test1.sql
@@ -9,4 +9,5 @@ BEGIN
     CALL SYSADMIN.createOptimizationJob(recOptId => optId, allowIndexCreation => 'ALL', gatherNativeStats => true, indexCreationByStatus => 'NEW_ACCEPTED', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, jobName => '0390a010-5a52-11eb-aebd-0a0027000015');
     CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='0390a010-5a52-11eb-aebd-0a0027000015'), type => 'CRON', startDelay => 0, enabled => true, cronExpression => '0 0 12 L * ?', scheduleName => '0aa0a3a1-5a52-11eb-aebd-0a0027000015');
     CALL SYSADMIN.createIncrementalOptimizationJob(recOptId => optId, newRowCheckExpression => '"a"', identityExpression => 'null', deleteOldData => true, gatherNativeStats => false, parallelRunsAllowed => 1, retryCounter => 3, retryDelay => 30, runTimeout => 0, jobName => '14b3fc22-5a52-11eb-aebd-0a0027000015');
+    CALL SYSADMIN.createSchedule(jobId => (SELECT id FROM SYSADMIN.ScheduleJobs WHERE jobName='14b3fc22-5a52-11eb-aebd-0a0027000015'), type => 'INTERVAL', startDelay => 1, enabled => true, intervl => 120, scheduleName => '19f02a13-5a52-11eb-aebd-0a0027000015');
 END;;
