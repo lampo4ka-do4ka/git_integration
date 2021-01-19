855b0157705c7fc429854a200e0628b875c0452d
admin <admin@datavirtuality>
2021-01-19 16:20:46 +0300

    Data Source excel has been dropped.

--- datasources/excel.sql
+++
@@ -1,2 +0,0 @@
-call SYSADMIN.createConnection(name => 'excel', jbossCliTemplateName => 'excel', connectionOrResourceAdapterProperties => 'ParentDirectory=E:/TestData', encryptedProperties => '');;
-call SYSADMIN.createDatasource(name => 'excel', translator => 'excel', modelProperties => 'importer.ExcelFileName=test_excel_xls.xls,importer.useFullSchemaName=false,importer.headerRowNumber=1', translatorProperties => '', encryptedModelProperties => '', encryptedTranslatorProperties => '');;
