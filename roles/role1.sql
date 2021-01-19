4e8667c3edac8e15be7452a508966731dc6ad18c
admin <admin@datavirtuality>
2021-01-19 15:22:24 +0300

    Permission ROLE1::PG.TEST1 has been dropped.

--- roles/role1.sql
+++ roles/role1.sql
@@ -1,4 +1,3 @@
 CALL SYSADMIN.addRole(name => 'role1', allowCreateTempTables  => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'pg.test1', permissions => 'CRUDEAL', isConstraint => true);;
 CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v1', permissions => 'CRUDE', condition => 'a = 1', isConstraint => true);;
 CALL SYSADMIN.setPermissions(role_name => 'role1', resourceName => 'views.v3', permissions => 'CRUDEA', isConstraint => false);;
