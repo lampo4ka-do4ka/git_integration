7aa3bc92cc4b5491b09bc4130c04ec33863f1e0c
admin <admin@datavirtuality>
2021-01-21 15:55:10 +0300

    Job has been dropped.

--- jobs/61297f41-5be5-11eb-a8b4-0a0027000015.sql
+++
@@ -1,4 +0,0 @@
-BEGIN
- DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select * from "a.v1" ', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => '[SQL script] select * from "a.v1" ', jobName => '61297f41-5be5-11eb-a8b4-0a0027000015') OPTION $NOFAIL;
-END;;
