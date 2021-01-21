3d8dd3e258d705c040f5d467673197619d368db1
admin <admin@datavirtuality>
2021-01-21 16:04:16 +0300

    User has been dropped.

--- users/user12.sql
+++
@@ -1,2 +0,0 @@
-CALL SYSADMIN.importUser(name => 'user12', encryptedPwd => '4TNj0DF02EY=', role_name => 'connect-dv-role');;
-CALL SYSADMIN.addUserRole(user_name => 'user12', role_name => 'role123');;
