291d7513e1aed3cbd3e930397d3e77b6137bb2b7
admin <admin@datavirtuality>
2021-01-21 16:02:09 +0300

    Permission ROLE12::PG.TEST1 has been changed.

--- roles/role12.sql
+++ roles/role12.sql
@@ -1,2 +1,2 @@
 CALL SYSADMIN.addRole(name => 'role12', allowCreateTempTables  => true);;
-CALL SYSADMIN.setPermissions(role_name => 'role12', resourceName => 'pg.test1', permissions => 'CRUDEA', isConstraint => true);;
+CALL SYSADMIN.setPermissions(role_name => 'role12', resourceName => 'pg.test1', permissions => 'CRUDE', isConstraint => true);;
