2d9499a96c3f238304ab9a476b04755e1cc58e44
admin <admin@datavirtuality>
2021-01-21 15:41:52 +0300

    Job has been renamed to 61297f41-5be5-11eb-a8b4-0a0027000015.

--- jobs/61297f41-5be5-11eb-a8b4-0a0027000015.sql
+++ jobs/61297f41-5be5-11eb-a8b4-0a0027000015.sql
@@ -1,5 +1,5 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
-    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select 1', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => 'x', jobName => '61297f41-5be5-11eb-a8b4-0a0027000015') OPTION $NOFAIL;
+    jobId = CALL SYSADMIN.CreateSQLJob(script => 'select * from "a.v1" ', parallelRunsAllowed => 1, retryCounter => 0, retryDelay => 0, runTimeout => 0, description => '[SQL script] select * from "a.v1" ', jobName => '61297f41-5be5-11eb-a8b4-0a0027000015') OPTION $NOFAIL;
     CALL SYSADMIN.createSchedule(jobId => jobId, type => 'INTERVAL', startDelay => 0, enabled => true, intervl => 60, scheduleName => 'cdb71be2-5be5-11eb-a8b4-0a0027000015');
 END;;
