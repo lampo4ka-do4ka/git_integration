89933ca96807a016ed829d870fbe3133671e928b
admin <admin@datavirtuality>
2021-01-25 00:49:30 +0300

    User user1 has been changed.

--- users/user1.sql
+++ users/user1.sql
@@ -1,2 +1,3 @@
 CALL SYSADMIN.importUser(name => 'user1', encryptedPwd => '4TNj0DF02EY=', role_name => 'admin-role');;
 CALL SYSADMIN.addUserRole(user_name => 'user1', role_name => 'connect-dv-role');;
+CALL SYSADMIN.addUserRole(user_name => 'user1', role_name => 'role1');;
