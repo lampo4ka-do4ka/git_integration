77f5d8d900c816f1f58f9be21e04da22a163cfba
admin <admin@datavirtuality>
2021-05-06 13:44:12 +0300

    Schedule fe59ae11-ae57-11eb-9552-0a0027000013 has been created.

--- jobs/git-push-job.sql
+++ jobs/git-push-job.sql
@@ -1,4 +1,5 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
     jobId = CALL SYSADMIN.gitIntegrationActivatePushJob();;
+    CALL SYSADMIN.createSchedule(jobId => jobId, type => 'CRON', startDelay => 0, enabled => true, cronExpression => '0 */1 * * * ?', scheduleName => 'fe59ae11-ae57-11eb-9552-0a0027000013');
 END;;
