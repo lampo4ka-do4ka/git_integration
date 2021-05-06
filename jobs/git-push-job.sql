3690fe135e2b29d3cd8ba68849f82eee45c55971
admin <admin@datavirtuality>
2021-05-06 13:44:11 +0300

    Job git-push-job has been created.

--- jobs/git-push-job.sql
+++ jobs/git-push-job.sql
@@ -1,5 +1,4 @@
 BEGIN
  DECLARE BIGINTEGER jobId;
     jobId = CALL SYSADMIN.gitIntegrationActivatePushJob();;
-    CALL SYSADMIN.createSchedule(jobId => jobId, type => 'CRON', startDelay => 0, enabled => true, cronExpression => '0 */1 * * * ?', scheduleName => '5ced8a0e-5e83-11eb-967d-0a0027000015');
 END;;
